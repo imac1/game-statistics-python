53835c02fb02a155a1f3bf8540985933237cb039
imac1 <irina.macovei@yahoo.fr>
2022-06-21 20:18:16 +0300

    reports -8

--- reports.py
+++ reports.py
@@ -2,12 +2,11 @@ import os.path
 import csv
 
 file_dir = os.path.dirname(os.path.realpath('__file__'))
-dir_path = 'PythonBasics/game-statistics-python'
 
 
 def count_games(file_name):
     count = 0
-    with open(os.path.join(file_dir, dir_path, file_name), 'r') as f:
+    with open(os.path.join(file_dir, file_name), 'r') as f:
         rows = f.readlines()
         for row in rows:
             count += 1
@@ -16,7 +15,7 @@ def count_games(file_name):
 
 
 def decide(file_name, year):
-    with open(os.path.join(file_dir, dir_path, file_name), 'r') as f:
+    with open(os.path.join(file_dir, file_name), 'r') as f:
         reader = csv.reader(f, delimiter="\t")
         years_col = list(zip(*reader))[2]
         f.close()
@@ -26,7 +25,7 @@ def decide(file_name, year):
 
 
 def get_latest(file_name):
-    with open(os.path.join(file_dir, dir_path, file_name), 'r') as f:
+    with open(os.path.join(file_dir, file_name), 'r') as f:
         reader = csv.reader(f, delimiter="\t")
         games_yr_dict = {rows[0]: rows[2] for rows in reader}
         max_year = max(games_yr_dict.values())
@@ -36,7 +35,7 @@ def get_latest(file_name):
     
 
 def count_by_genre(file_name, genre):
-    with open(os.path.join(file_dir, dir_path, file_name), 'r') as f:
+    with open(os.path.join(file_dir, file_name), 'r') as f:
         reader = csv.reader(f, delimiter="\t")
         games_genre_dict = [rows[3] for rows in reader if rows[3].lower() == genre.lower()]
         f.close()
@@ -44,7 +43,7 @@ def count_by_genre(file_name, genre):
 
 
 def get_line_number_by_title(file_name, title):
-    with open(os.path.join(file_dir, dir_path, file_name), 'r') as f:
+    with open(os.path.join(file_dir, file_name), 'r') as f:
         reader = csv.reader(f, delimiter='\t')
         title_list = [row[0] for row in reader]
         for i, value in enumerate(title_list):
@@ -55,19 +54,37 @@ def get_line_number_by_title(file_name, title):
 
 
 def sort_abc(file_name):
-    
+    with open(os.path.join(file_dir, file_name), 'r') as f:
+        reader = csv.reader(f, delimiter='\t')
+        sorted_list = [row[0] for row in reader]
+        for i in range(len(sorted_list)):
+            for j in range(i+1, len(sorted_list)):
+                if sorted_list[i] > sorted_list[j]:
+                    sorted_list[i], sorted_list[j] = sorted_list[j], sorted_list[i]
+        return sorted_list
 
 
 def get_genres(file_name):
-    pass
+    with open(os.path.join(file_dir, file_name), 'r') as f:
+        reader = csv.reader(f, delimiter='\t')
+        sorted_set = set(row[3] for row in reader)
+        sorted_list = list(sorted_set)
+        for i in range(len(sorted_list)):
+            for j in range(i+1, len(sorted_list)):
+                if sorted_list[i] > sorted_list[j]:
+                    sorted_list[i], sorted_list[j] = sorted_list[j], sorted_list[i]
+        return sorted_list
 
 
 def when_was_top_sold_fps(file_name):
-    pass
+
 
 
 if __name__ == '__main__':
     # print(count_games('game_stat.txt'))
     # print(decide('game_stat.txt', 2000))
     # print(count_by_genre('game_stat.txt', genre='Real-time strategy'))
-    print(get_line_number_by_title('game_stat.txt', 'StarCraft II'))
+    # print(get_line_number_by_title('game_stat.txt', 'StarCraft II'))
+    # print(sort_abc('game_stat.txt'))
+    # print(get_genres('game_stat.txt'))
+    print(when_was_top_sold_fps('game_stat.txt'))
